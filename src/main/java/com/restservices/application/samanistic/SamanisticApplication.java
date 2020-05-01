56e5fbf6763c71a7aac975ef0a3f919e5f5169e6
Siddharth Majhi <majhysiddharth17121996@gmail.com>
2020-05-01 18:15:47 +0530

    First Commit

---
+++ src/main/java/com/restservices/application/samanistic/SamanisticApplication.java
@@ -0,0 +1,13 @@
+package com.restservices.application.samanistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamanisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamanisticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restservices/application/samanistic/SamanisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restservices.application.samanistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamanisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
